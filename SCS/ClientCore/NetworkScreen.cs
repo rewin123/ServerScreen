fcf032269f42f3a5fe2e0ec49bc4d905cac4dcb9
rewin <rewin@DESKTOP-TL232GJ>
2017-12-10 14:54:43 +0300

    Добавил прозрачный NetworkScreen

---
+++ SCS/ClientCore/NetworkScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCore
+{
+    public class NetworkScreen : MarshalByRefObject
+    {
+        public byte[] GetScreen()
+        {
+            MemoryStream mem = new MemoryStream();
+             Screenshot.MakeScreenshot().Save(mem,ImageFormat.Png);
+            byte[] arr = new byte[mem.Length];
+            mem.Position = 0;
+            mem.Read(arr, 0, (int)mem.Length);
+            return arr;
+        }
+    }
+}
--- SCS/SCSTest/Form1.cs
+++ SCS/SCSTest/Form1.cs
@@ -10,12 +10,16 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using ClientCore;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.IO;
 
 namespace SCSTest
 {
     public partial class Form1 : Form
     {
-        ScreenStream screen;
+        NetworkScreen screen;
         int port = 8996;
         public Form1()
         {
@@ -25,25 +29,27 @@ namespace SCSTest
 
         private void Button1_Click_1(object sender, EventArgs e)
         {
-            TcpListener listener = TcpListener.Create(port);
-            listener.Start();
-            TcpClient client = listener.AcceptTcpClient();
-            NetworkStream str = client.GetStream();
-            screen = new ClientCore.ScreenStreamSend(str);
-            timer1.Start();
+            ChannelServices.RegisterChannel(new TcpChannel(port),false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(NetworkScreen), "NetworkScreen.rem", WellKnownObjectMode.SingleCall);
+            screen = new NetworkScreen();
+
+            //timer1.Start();
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            screen.Tick();
+            MemoryStream mem = new MemoryStream();
+            byte[] arr = screen.GetScreen();
+            mem.Write(arr, 0, arr.Length);
+            mem.Position = 0;
+            pictureBox1.Image = new Bitmap(mem);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            TcpClient client = new TcpClient(textBox1.Text, port);
-            
-            
-            screen = new ScreenStreamGet(client.GetStream(), pictureBox1);
+            RemotingConfiguration.RegisterWellKnownClientType(typeof(NetworkScreen), "tcp://localhost:" + port + "/NetworkScreen.rem");
+            screen = new NetworkScreen();
+
             timer1.Start();
         }
     }
